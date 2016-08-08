c68690661a5f9bbbd10eb1408495789abbae984b
Aulas <Aulas>
2016-08-08 15:33:03 -0300

    Aula 10 - Selecionando Dados

---
+++ Basico/Aula 10 - Selecionando Dados Parte 1.sql
@@ -0,0 +1,12 @@
+-- Selecionando dados das tabelas
+-- SELECT [Colunas] FROM [tabela]
+
+
+
+
+SELECT nmProduto, cdProduto, vlProduto FROM tb_Produtos;
+
+SELECT cdProduto from tb_Produtos
+
+SELECT cdProduto, nmProduto, vlProduto FROM tb_Produtos;
+SELECT * FROM tb_Vendas
