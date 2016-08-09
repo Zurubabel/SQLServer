8362ba8bd42a5f93cde382ec7802037939f62ae6
Aulas <Aulas>
2016-08-08 21:22:24 -0300

    Aula 12 - Populando a tabela de vendas e de clientes

---
+++ Basico/Aula 12 - Populando a tabela de vendas e de clientes.sql
@@ -0,0 +1,15 @@
+-- Inserindo novos produtos
+
+--SELECT * FROM tb_Produtos;
+--SELECT * FROM tb_Vendas
+--SELECT * FROM tb_Clientes;
+--SELECT * FROM tb_ProdutoVenda
+
+-- 1 - Criar um cliente
+--INSERT INTO tb_Clientes VALUES ('Paulo Guina', '1980-05-01', 'M', 'Rua da Mecânica, 400', 'Campinas', 'São Paulo', '11 1234-5678', '11 912345678');
+
+-- 2 - Insert da venda
+--INSERT INTO tb_Vendas VALUES (1, GETDATE());
+
+-- 3 - Insert da venda
+--INSERT INTO tb_ProdutoVenda VALUES (1, 1, 3);
