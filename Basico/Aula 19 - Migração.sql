522d1d870938f0f1c380f3f5af49d1573e3a150e
Aulas <Aulas>
2016-08-26 00:28:50 -0300

    Aula 19 - Migração

---
+++ Basico/Aula 19 - Migração.sql
@@ -0,0 +1,7 @@
+/*
+	INSERT INTO tb_Clientes VALUES ('José Penha', '1977-05-07', 'M', 'Rua da Mecânica, 33', 'Campinas', 'São Paulo', '11 1233-4434', '11 99283-9099')
+	INSERT INTO tb_Clientes VALUES ('Zequinha Muriçoca', '1988-05-07', 'M', 'Rua Jambira, 13', 'Campinas', 'São Paulo', '11 1584-2214', '')
+	INSERT INTO tb_Clientes VALUES ('Mariazinha de Lá', '1981-05-07', 'F', 'Rua Jambira, 13', 'Campinas', 'São Paulo', '11 1584-2214', '')
+*/
+
+select * from tb_Clientes
