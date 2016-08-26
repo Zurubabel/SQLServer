f3f2f624e0b8dfb9d3e7c7cbbbc5616534b2cb49
Aulas <Aulas>
2016-08-26 00:48:24 -0300

    Aula 21 - Criando constraints das novas tabelas e correção do arquivo da aula 20

--- Basico/Aula 21 - Criando Constraints das Novas Tabelas.sql
+++ Basico/Aula 21 - Criando Constraints das Novas Tabelas.sql
@@ -1,12 +1,14 @@
 CREATE TABLE tb_Enderecos (
 	cdEndereco INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	nmEndereco VARCHAR(50) NOT NULL,
-	nmCEP VARCHAR(9) 
+	nmCEP VARCHAR(9),
+	cdCidade INT NOT NULL
 )
 
 CREATE TABLE tb_Cidades (
 	cdCidade INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-	nmCidade VARCHAR(50) NOT NULL
+	nmCidade VARCHAR(50) NOT NULL,
+	cdEstado INT NOT NULL
 )
 
 CREATE TABLE tb_Estados (
@@ -14,3 +16,13 @@ CREATE TABLE tb_Estados (
 	nmEstado VARCHAR(30) NOT NULL,
 	nmSigla VARCHAR(2) NOT NULL
 )
+
+ALTER TABLE tb_Enderecos
+ADD CONSTRAINT FK_Enderecos_Cidades
+FOREIGN KEY (cdCidade)
+REFERENCES tb_Cidades (cdCidade);
+
+ALTER TABLE tb_Cidades
+ADD CONSTRAINT FK_Cidades_Estados
+FOREIGN KEY (cdEstado)
+REFERENCES tb_Estados (cdEstado);
