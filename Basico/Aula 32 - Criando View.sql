f2f52b0da52801447861603724843bee482fb9df
Aulas <Aulas>
2016-11-25 00:01:21 -0200

    Aula 32 - Criando Views

---
+++ Basico/Aula 32 - Criando View.sql
@@ -0,0 +1,21 @@
+CREATE VIEW vw_Endereco_Completo_Clientes
+AS
+SELECT 
+	CL.cdCliente,
+	CL.nmCliente,
+	CL.dtNascimento,
+	CL.inSexo,
+	EN.cdEndereco,
+	EN.nmEndereco,
+	EN.nmCEP,
+	CI.cdCidade,
+	CI.nmCidade,
+	E.cdEstado,
+	E.nmEstado,
+	E.nmSigla
+FROM tb_Clientes CL INNER JOIN tb_Enderecos EN ON CL.cdEndereco = EN.cdEndereco
+INNER JOIN tb_Cidades CI ON EN.cdCidade = CI.cdCidade
+
+
+/*SELECT * FROM vw_Endereco_Completo_Clientes
+INNER JOIN tb_Estados E ON CI.cdEstado = E.cdEstado;*/
